cda524d268c39d72f04c59d8cc717d360bfeccc0
admin <raviukolte@gmail.com>
2017-07-08 12:05:34 +0530

    ravi testbase added click method and webdriver instance

--- rk/src/test/java/base/TestBase.java
+++ rk/src/test/java/base/TestBase.java
@@ -1,7 +1,17 @@
 package base;
 
-public class TestBase {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public class TestBase {
 	
 	
+	public static WebDriver driver;
+	
+	
+	
+	public void Click(String locator)
+	{
+		driver.findElement(By.xpath("locator")).click();
+	}
 }
